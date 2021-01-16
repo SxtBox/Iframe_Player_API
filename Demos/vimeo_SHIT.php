8b934d38db16283efff4b4c2da3b90cd7c8bb8fa
Albdroid.AL <sxt@usa.com>
2021-01-16 17:03:33 +0100

    Update vimeo_SHIT.php

--- Demos/vimeo_SHIT.php
+++ Demos/vimeo_SHIT.php
@@ -52,7 +52,6 @@ $URL = "https://vimeo.com/493206257";
 $API_CALL = $API_HOST. $CALL_URL . $URL . $Parameter . $Type;
 
 $DATA = file_get_contents($API_CALL);
-$GET_URL = file_get_contents($API_CALL.$_SERVER['QUERY_STRING']);
-
+// $GET_URL = file_get_contents($API_CALL.$_SERVER['QUERY_STRING']);
 echo $DATA;
 ?>
